e5da18b9752750cffaf6a74966dada6c9cb8565d
Antonio Melé <antonio.mele@gmail.com>
2021-10-03 19:33:19 +0200

    Remove social URL pattern

--- Chapter04/bookmarks/bookmarks/urls.py
+++ Chapter04/bookmarks/bookmarks/urls.py
@@ -22,8 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('social-auth/',
-         include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
