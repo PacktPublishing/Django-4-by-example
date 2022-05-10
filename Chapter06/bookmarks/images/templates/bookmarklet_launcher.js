e2a55dc6e9c859b311ec5b7036b328f06cae3a8e
Antonio Melé <antonio.mele@gmail.com>
2022-05-10 22:35:47 +0200

    Adjust bookmarklet_launcher param digits

--- Chapter06/bookmarks/images/templates/bookmarklet_launcher.js
+++ Chapter06/bookmarks/images/templates/bookmarklet_launcher.js
@@ -1,7 +1,7 @@
 (function(){
   if(!window.bookmarklet) {
     bookmarklet_js = document.body.appendChild(document.createElement('script'));
-    bookmarklet_js.src='//127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*999999999999999999);
+    bookmarklet_js.src='//127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*9999999999999999);
     window.bookmarklet = true;
   }
   else {
