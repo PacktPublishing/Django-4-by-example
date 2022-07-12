31ce0fad5c9f22297364db8484846a95483cd309
Antonio Melé <antonio.mele@gmail.com>
2022-07-12 19:26:52 +0200

    Removed explicit object inheritance

--- Chapter10/myshop/shop/recommender.py
+++ Chapter10/myshop/shop/recommender.py
@@ -9,7 +9,7 @@ r = redis.Redis(host=settings.REDIS_HOST,
                 db=settings.REDIS_DB)
 
 
-class Recommender():
+class Recommender:
     def get_product_key(self, id):
         return f'product:{id}:purchased_with'
 
--- Chapter13/educa/courses/views.py
+++ Chapter13/educa/courses/views.py
@@ -23,13 +23,13 @@ class ManageCourseListView(ListView):
         return qs.filter(owner=self.request.user)
 
 
-class OwnerMixin(object):
+class OwnerMixin:
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
 
 
-class OwnerEditMixin(object):
+class OwnerEditMixin:
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
