924a06609b450a69bcda1728e61cd7d7309dbb31
kstingel <ksgit@kstingel.net>
2022-08-10 18:57:35 +1000

    07 Recommending posts by email

---
+++ Snippets/ch02/07.01/blog/forms_01.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)
