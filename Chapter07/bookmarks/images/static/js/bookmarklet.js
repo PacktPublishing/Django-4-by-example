ca84368ea883ecd381d9ff048eb78486b5e5cd2e
Antonio Melé <antonio.mele@gmail.com>
2022-08-17 20:50:39 +0200

    bookmarklet javascript var to const

--- Chapter07/bookmarks/images/static/js/bookmarklet.js
+++ Chapter07/bookmarks/images/static/js/bookmarklet.js
@@ -1,7 +1,7 @@
-var siteUrl = '//127.0.0.1:8000/';
-var styleUrl = siteUrl + 'static/css/bookmarklet.css';
-var minWidth = 250;
-var minHeight = 250;
+const siteUrl = '//127.0.0.1:8000/';
+const styleUrl = siteUrl + 'static/css/bookmarklet.css';
+const minWidth = 250;
+const minHeight = 250;
 
 // load CSS
 var head = document.getElementsByTagName('head')[0];  // Get HTML head element
